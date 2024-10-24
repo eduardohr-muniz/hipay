bc398a834f5501ca9a936ab6e88f523c8787d2cf
eduardohr-muniz <eduardohr.muniz@outlook.com>
2024-10-24 20:24:42 -0300

    feat: architeture

--- lib/hipay.dart
+++ lib/hipay.dart
@@ -1,7 +1,9 @@
 library hipay;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Hipay {
+  final String apiKey;
+  final bool sandbox;
+  final bool enableLogs;
+
+  Hipay({required this.apiKey, this.sandbox = false, this.enableLogs = false});
 }
---
+++ lib/src/hipay_repository.dart
@@ -0,0 +1,3 @@
+import 'package:hipay/src/i_hipay_repository.dart';
+
+class HipayRepository implements IHipayRepository {}
